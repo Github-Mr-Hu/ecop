e72c6104c192371054f1b3efd94f51a4d05f8cdc
Mr.Hu <https://github.com/Github-Mr-Hu/ecop>
2020-08-25 15:58:05 +0800

    测试

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName test
+ * @Description TODO
+ * @Author hufw
+ * @Date 2020/8/25 15:50
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test!");
+    }
+}
